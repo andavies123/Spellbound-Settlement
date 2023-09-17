2ce1f2ee8b590c427e8107905c70c52f11acb429
Andrew Davies <davies.andrew.123@gmail.com>
2023-09-17 16:41:17 -0700

    Added example for drawing a mesh face

--- SpellboundSettlement/MainGame.cs
+++ SpellboundSettlement/MainGame.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using SpellboundSettlement.Global;
 using SpellboundSettlement.Inputs;
@@ -53,9 +54,55 @@ public class MainGame : Game
 	protected override void Draw(GameTime gameTime)
 	{
 		GraphicsDevice.Clear(Color.CornflowerBlue);
+		
+		DrawSquare();
 
 		// TODO: Add your drawing code here
 
 		base.Draw(gameTime);
+	}
+
+	private void DrawSquare()
+	{
+		Matrix worldMatrix = Matrix.CreateTranslation(new Vector3(0, 0, 0));
+		Matrix viewMatrix = Matrix.CreateLookAt(new Vector3(0, 10, 10), Vector3.Zero, Vector3.Up);
+		Matrix projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 1f, 100f);
+		Effect effect = Content.Load<Effect>("TestShader");
+		
+		VertexPositionColor[] vertices =
+		{
+			new(new Vector3(0, 0, 0), Color.Aqua),
+			new(new Vector3(1, 0, 0), Color.Red),
+			new(new Vector3(0, 1, 0), Color.Yellow),
+			new(new Vector3(1, 1, 0), Color.Green),
+		};
+
+		short[] indices =
+		{
+			2, 1, 0,
+			3, 1, 2
+		};
+
+		VertexBuffer vertexBuffer = new(GraphicsDevice, typeof(VertexPositionColor), vertices.Length, BufferUsage.None);
+		vertexBuffer.SetData(vertices);
+
+		IndexBuffer indexBuffer = new(GraphicsDevice, IndexElementSize.SixteenBits, indices.Length, BufferUsage.None);
+		indexBuffer.SetData(indices);
+		
+		GraphicsDevice.SetVertexBuffer(vertexBuffer);
+		GraphicsDevice.Indices = indexBuffer;
+		
+		// effect.Parameters["World"].SetValue(worldMatrix);
+		// effect.Parameters["View"].SetValue(viewMatrix);
+		// effect.Parameters["Projection"].SetValue(projectionMatrix);
+		effect.Parameters["WorldViewProjection"].SetValue(worldMatrix * viewMatrix * projectionMatrix);
+		
+		effect.CurrentTechnique.Passes[0].Apply();
+		
+		GraphicsDevice.DrawIndexedPrimitives(
+			PrimitiveType.TriangleList,
+			0,
+			0,
+			indexBuffer.IndexCount / 3);
 	}
 }
