d872ed949e2c35adf8ce5ec459bc377337ccbcf4
Andrew Davies <davies.andrew.123@gmail.com>
2023-10-02 19:22:52 -0700

    Added some base classes for UI

--- GameUI/Button.cs
+++ GameUI/Button.cs
@@ -1,11 +1,31 @@
 ﻿namespace GameUI;
 
-public class Button
+public class Button : UIElement
 {
-	public Button(string text)
+	public event Action MouseEntered;
+	public event Action MouseExited;
+	public event Action ButtonClicked;
+	
+	public Button((float width, float height) size, (float x, float y) position, string text) : 
+		base(size, position)
 	{
 		Text = text;
 	}
 
+	/// <summary>
+	/// The text that is displayed on the button
+	/// </summary>
 	public string Text { get; set; }
+	
+	/// <summary>
+	/// True if the button currently has focus
+	/// False if the button does not currently have focus
+	/// </summary>
+	public bool IsFocusedOn { get; set; } = false;
+
+	/// <summary>
+	/// True if the button can be clicked
+	/// False if the button can not be clicked
+	/// </summary>
+	public bool IsClickable { get; set; } = true;
 }
---
+++ GameUI/UIElement.cs
@@ -0,0 +1,28 @@
+﻿namespace GameUI;
+
+public abstract class UIElement
+{
+	protected UIElement(
+		(float width, float height) size,
+		(float x, float y) position)
+	{
+		Size = size;
+		Position = position;
+	}
+
+	/// <summary>
+	/// True if the UI element is visible
+	/// False if the UI element is not visible
+	/// </summary>
+	public bool IsVisible { get; set; } = true;
+	
+	/// <summary>
+	/// The width and height of the UI element
+	/// </summary>
+	public (float width, float height) Size { get; set; }
+	
+	/// <summary>
+	/// The position that the UI element will be displayed anchored from the top left corner
+	/// </summary>
+	public (float x, float y) Position { get; set; }
+}
