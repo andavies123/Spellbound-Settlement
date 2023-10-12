c92594cd6535055722954ec53275839e22dbd58c
Andrew Davies <davies.andrew.123@gmail.com>
2023-10-12 11:34:01 -0700

    Added base IStateMachine and IState classes

---
+++ SpellboundSettlement/StateMachines/IState.cs
@@ -0,0 +1,9 @@
+﻿namespace SpellboundSettlement.StateMachines;
+
+public interface IState
+{
+	void Init();
+	void BeginState();
+	void Update();
+	void EndState();
+}
---
+++ SpellboundSettlement/StateMachines/IStateMachine.cs
@@ -0,0 +1,9 @@
+﻿namespace SpellboundSettlement.StateMachines;
+
+public interface IStateMachine<T> where T : IState
+{
+	T CurrentState { get; }
+
+	void SetCurrentState(T nextState);
+	void UpdateCurrentState();
+}
