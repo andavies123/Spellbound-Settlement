8e5dcdab5d0d00ff2ac5c4ba39bb020621077700
Andrew Davies <davies.andrew.123@gmail.com>
2023-11-19 09:31:29 -0800

    Temporarily updated way to get the server exe for both windows and mac

--- Andavies.MonoGame.Game.Server/ServerStarter.cs
+++ Andavies.MonoGame.Game.Server/ServerStarter.cs
@@ -5,12 +5,15 @@ namespace Andavies.MonoGame.Game.Server;
 
 public class ServerStarter : IServerStarter
 {
+	private const string WindowsServerPath = "Andavies.MonoGame.Game.Server.exe";
+	private const string MacServerPath = "Andavies.MonoGame.Game.Server";
+	
 	public void StartServer(string ipAddress)
 	{
-		Console.WriteLine(GetServerExePath());
+		Console.WriteLine(GetServerExecutablePath());
 		ProcessStartInfo startInfo = new()
 		{
-			FileName = GetServerExePath(),
+			FileName = GetServerExecutablePath(),
 			UseShellExecute = true,
 			CreateNoWindow = false
 		};
@@ -18,10 +21,19 @@ public class ServerStarter : IServerStarter
 		Process.Start(startInfo);
 	}
 
-	private static string GetServerExePath()
+	private static string GetServerExecutablePath()
 	{
 		string assemblyLocation = Assembly.GetExecutingAssembly().Location;
 		string assemblyDirectory = Path.GetDirectoryName(assemblyLocation) ?? string.Empty;
-		return Path.Combine(assemblyDirectory, "Andavies.MonoGame.Game.Server.exe");
+		
+		//Todo: Find better way to get the assembly path, this should be temporary
+		string path = Path.Combine(assemblyDirectory, WindowsServerPath);
+		if (File.Exists(path))
+			return path;
+		path = Path.Combine(assemblyDirectory, MacServerPath);
+		if (File.Exists(path))
+			return path;
+
+		return string.Empty;
 	}
 }
