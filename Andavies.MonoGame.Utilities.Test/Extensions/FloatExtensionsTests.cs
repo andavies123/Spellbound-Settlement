91248cdd0713ac1358419212dd0cfebf6cf9f932
Andrew Davies <davies.andrew.123@gmail.com>
2024-02-28 19:25:34 -0800

    Created FloatExtensionsTests.cs

---
+++ Andavies.MonoGame.Utilities.Test/Extensions/FloatExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿using Andavies.MonoGame.Utilities.Extensions;
+
+namespace Andavies.MonoGame.Utilities.Test.Extensions;
+
+[TestClass]
+public class FloatExtensionsTests
+{
+	#region GetTenthsPlace Tests
+
+	[DataTestMethod]
+	[DataRow(1.234f, 2)]
+	[DataRow(75.468f, 4)]
+	[DataRow(972.249f, 2)]
+	[DataRow(-1.237f, 2)]
+	public void GetTenthsPlace_ReturnsCorrectNumber(float value, int expected)
+	{
+		// Act
+		int answer = value.GetTenthsPlace();
+
+		// Assert
+		answer.Should().Be(expected);
+	}
+
+	#endregion
+	
+	#region GetTenthsPlace Tests
+
+	[DataTestMethod]
+	[DataRow(1.234f, 3)]
+	[DataRow(75.468f, 6)]
+	[DataRow(972.249f, 4)]
+	[DataRow(-1.237f, 3)]
+	public void GetHundredthsPlace_ReturnsCorrectNumber(float value, int expected)
+	{
+		// Act
+		int answer = value.GetHundredthsPlace();
+
+		// Assert
+		answer.Should().Be(expected);
+	}
+
+	#endregion
+	
+	#region GetThousandthsPlace Tests
+
+	[DataTestMethod]
+	[DataRow(1.234f, 4)]
+	[DataRow(75.468f, 8)]
+	[DataRow(972.249f, 9)]
+	[DataRow(-1.237f, 7)]
+	public void GetThousandthsPlace_ReturnsCorrectNumber(float value, int expected)
+	{
+		// Act
+		int answer = value.GetThousandthsPlace();
+
+		// Assert
+		answer.Should().Be(expected);
+	}
+
+	#endregion
+}
